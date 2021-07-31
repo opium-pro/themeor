fb1cb5e2f7433d09d38421551dfba04237f41da1
Opium Pro <ilove@opium.pro>
2021-08-01 00:51:23 +0200

    hotfix for gaps in align

--- src/themeor/utils/change-css-value.ts
+++ src/themeor/utils/change-css-value.ts
@@ -13,6 +13,7 @@ export const half = (value?: string | number | false) => {
 
   const splitted = splitValue(value) || [0]
   return [splitted[0] / 2, splitted[1]].join('')
+  
 }
 
 export const minus = (value?: string | number | false) => {
@@ -25,7 +26,7 @@ export const minus = (value?: string | number | false) => {
 
 export const splitValue = (value: string | number): any[] => {
   const number = typeof value === 'number' ? value : parseInt(value)
-  const indexOfMeasurement = typeof value === 'number' ? 0 : value.toString().indexOf(number.toString())
+  const indexOfMeasurement = typeof value === 'number' ? 0 : number.toString().length
   const measurement = typeof value === 'number' ? '' : value.toString().slice(indexOfMeasurement, value.length)
 
   return [number, measurement]
