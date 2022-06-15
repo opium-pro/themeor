643b38cd628661d754bf3c441156a5d0efc02f47
Denis Elianovsky <denis@opium.pro>
2022-06-15 04:55:39 +0200

    fit fix

--- src/themeor/utils/change-css-value.ts
+++ src/themeor/utils/change-css-value.ts
@@ -1,33 +1,27 @@
 // Get CSS values from strings and makes some changes with it
 
 export const double = (value?: string | number | false) => {
-  if (!value) {return}
-
-  const splitted = splitValue(value)
-  return [splitted[0] * 2, splitted[1]].join('')
+  if (!value) { return }
+  return changeValue(value, v => v * 2)
 }
 
 
 export const half = (value?: string | number | false) => {
-  if (!value) {return}
-
-  const splitted = splitValue(value) || [0]
-  return [splitted[0] / 2, splitted[1]].join('')
-  
+  if (!value) { return }
+  return changeValue(value, v => v / 2)
 }
 
 export const minus = (value?: string | number | false) => {
-  if (!value) {return}
-
-  const splitted = splitValue(value) || [0]
-  return [-splitted[0], splitted[1]].join('')
+  if (!value) { return }
+  return changeValue(value, v => -v)
 }
 
 
-export const splitValue = (value: string | number): any[] => {
-  const number = typeof value === 'number' ? value : parseInt(value)
+export const changeValue = (value: string | number = 0, action: (value: number) => number = v => v): string | number => {
+  const isNumber = typeof value === 'number'
+  const number = isNumber ? value : parseInt(value)
   const indexOfMeasurement = typeof value === 'number' ? 0 : number.toString().length
   const measurement = typeof value === 'number' ? '' : value.toString().slice(indexOfMeasurement, value.length)
-
-  return [number, measurement]
+  const result = action(number)
+  return isNumber ? result : `${result}${measurement}`
 }
