49588e2668104f9a96416b38f06ee5c17d367171
Denis Elianovsky <denis@opium.pro>
2022-06-20 23:38:03 +0200

    reset fix

--- src/themeor/Theme/reset.ts
+++ src/themeor/Theme/reset.ts
@@ -83,12 +83,11 @@ const styles = {
     },
 
     [`input[type="search"]::-webkit-search-decoration,
-    input[type="search"]:: -webkit-search-cancel-button,
-    input[type="search"]:: -webkit - search - results - button,
-    input[type="search"]:: -webkit-search-results-decoration`]: {
+    input[type="search"]::-webkit-search-cancel-button,
+    input[type="search"]::-webkit-search-results-button,
+    input[type="search"]::-webkit-search-results-decoration`]: {
       display: 'none',
     },
-
   },
 }
 
