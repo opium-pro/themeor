7ad970779a6487c8b75b326128004a2517301a8c
Denis Elianovsky <denis@opium.pro>
2022-08-17 04:43:38 +0200

    switching to lower case

--- src/themeor/Align/index.tsx
+++ src/themeor/Align/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { default as Align } from './align.js'
-import { default as Span } from './Span.js'
+import { default as Span } from './span.js'
 import { AlignProps } from './types.js'
 
 Align.Span = Span
